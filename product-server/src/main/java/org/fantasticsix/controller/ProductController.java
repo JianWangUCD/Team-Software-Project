406b863410d270f048403078215f112b6e72927d
Qiongyi Zhang <zhangqiongyi97@163.com>
2023-07-10 22:44:35 +0100

    cros origin success, Feign(stock url) has problm

--- product-server/src/main/java/org/fantasticsix/controller/ProductController.java
+++ product-server/src/main/java/org/fantasticsix/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+//@CrossOrigin("http://localhost:3000")
 @Slf4j
 public class ProductController {
 
--- user-server/src/main/java/org/fantasticsix/controller/UserController.java
+++ user-server/src/main/java/org/fantasticsix/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+//@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
