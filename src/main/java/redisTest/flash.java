c785f0a4063da6a205f458e01bb21cb022982e1c
Mason <hsc886@outlook.com>
2023-07-13 23:55:46 +0100

    redis test

---
+++ src/main/java/redisTest/flash.java
@@ -0,0 +1,31 @@
+package redisTest;
+
+import org.redisson.Redisson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flash {
+
+    @Autowired
+    private Redisson redisson;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/deduct_stock")
+    public String deductStock() {
+
+        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+        if (stock > 0) {
+            int realStock = stock - 1;
+            stringRedisTemplate.opsForValue().set("stock", realStock + "");
+            System.out.println(realStock);
+        }else {
+            System.out.println("failed");
+        }
+        return "end";
+    }
+
+}
---
+++ src/test/java/com/mason/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mason.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
