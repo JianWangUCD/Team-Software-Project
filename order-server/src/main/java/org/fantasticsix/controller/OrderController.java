2111f71c6b1b3c62d52b2733a7ad1f7048347f03
GersRan <113507485+GersRan@users.noreply.github.com>
2023-07-13 21:53:09 +0100

    Add files via upload
    products里加了根据sellerId返回products的方法，order里加了根据userId返回orders的接口

---
+++ order-server/src/main/java/org/fantasticsix/controller/OrderController.java
@@ -0,0 +1,75 @@
+package org.fantasticsix.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fantasticsix.domain.Order;
+import org.fantasticsix.domain.Product;
+import org.fantasticsix.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+//@CrossOrigin("http://localhost:3000")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/flashsale/orders")
+    public List<Order> getAllOrders() {
+
+        List<Order> orders = orderService.getAllOrders();
+
+        return orders;
+    }
+
+    @GetMapping("/flashsale/user/{userId}/orders")
+    public List<Order> getOrdersByUserId(@PathVariable Long userId) {
+        return orderService.getOrdersByUserId(userId);
+    }
+
+
+
+    //search by id
+    @GetMapping("/flashsale/orders/{id}")
+    public Order getOrder(@PathVariable long id) {
+
+        Order order = orderService.getOrder(id);
+        return order;
+    }
+
+    @DeleteMapping("/flashsale/orders/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable long id) {
+
+        orderService.deleteOrder(id);
+
+        ResponseEntity<Void> responseEntity = ResponseEntity.noContent().build();
+        return responseEntity;
+    }
+
+    @PutMapping("/flashsale/orders/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable long id,
+                                             @RequestBody Order order) {
+
+        Order orderUpdated = orderService.updateOrder(id, order);
+
+        ResponseEntity<Order> responseEntity = new ResponseEntity<Order>(orderUpdated, HttpStatus.OK);
+
+        return responseEntity;
+    }
+
+
+    @RequestMapping("/flashsale/stock-check")
+    public Boolean stockCheck(long productId){
+        return orderService.stockCheck(productId);
+    }
+
+    @PostMapping("/flashsale/checkout")
+    public Order createOrder(long productId, long userId) {
+        return orderService.createOrder(productId, userId);
+    }
+}
---
+++ product-server/src/main/java/org/fantasticsix/service/ProductService.java
@@ -0,0 +1,24 @@
+package org.fantasticsix.service;
+
+import org.fantasticsix.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+
+    List<Product> getProductsBySeller(Long sellerId);
+
+    Product getProduct(long id);
+
+    void deleteProduct(long id);
+
+    Product updateProduct(long id, Product product);
+
+    Product createProduct(Product productRequest);
+
+    //Set<Product> addProductsToOrder(long orderId, Set<Product> productRequestList);
+    //Product addProductsToOrder(long orderId, Product productRequest);
+
+}
