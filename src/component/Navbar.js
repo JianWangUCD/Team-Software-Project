1dd84aefe21706f1238d3c5633a0db5370da8ba8
Qiongyi Zhang <zhangqiongyi97@163.com>
2023-07-23 17:14:02 +0100

    change navbar details

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,8 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom';
 import { logout } from '../action/authAction';
 
-
-import { Button, Nav, Navbar as BootstrapNavbar } from 'react-bootstrap';
+import { Container, NavDropdown, Nav, Navbar as BootstrapNavbar } from 'react-bootstrap';
 
 function Navbar() {
   const dispatch = useDispatch();
@@ -17,15 +16,52 @@ function Navbar() {
     dispatch(logout());
   };
 
+  if (userInfo && userInfo.role === "seller") 
   return (
     <div>
-      <BootstrapNavbar bg="light" expand="lg" sticky="top" className="py-3">
+      <BootstrapNavbar bg="light" expand="lg" sticky="top" className="py-3" 
+                collapseOnSelect>
+        <Container>
+        <BootstrapNavbar.Brand as={NavLink} to="/seller" className="fw-bold fs-3 px-2">
+          Flash Sale: {userInfo.username}
+        </BootstrapNavbar.Brand>
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <BootstrapNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto" >
+            <Nav.Item>
+              <NavLink className="nav-link" to="/seller">
+              Manage products
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink className="nav-link" to="/addProduct">
+                Upload Products
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+            <NavLink className="nav-link" onClick={logoutHandler}>
+                   Logout
+                 </NavLink>
+            </Nav.Item>
+          </Nav>
+          </BootstrapNavbar.Collapse>
+        </Container>
+      </BootstrapNavbar>
+      </div>
+  ) 
+
+  return (
+    <div>
+      <BootstrapNavbar bg="light" expand="lg" sticky="top" className="py-3" 
+                collapseOnSelect>
+        <Container>
         <BootstrapNavbar.Brand as={NavLink} to="/" className="fw-bold fs-3 px-2">
           Flash Sale
         </BootstrapNavbar.Brand>
-        <BootstrapNavbar.Toggle aria-controls="navbarSupportedContent" />
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <BootstrapNavbar.Collapse id="navbarSupportedContent">
+        <BootstrapNavbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
             <Nav.Item>
               <NavLink className="nav-link" to="/">
@@ -43,111 +79,43 @@ function Navbar() {
               </NavLink>
             </Nav.Item>
           </Nav>
-          {userInfo ? (
-            <div className="buttons text-center">
-              <Button variant="outline-dark" className="m-2">
-                {userInfo.username}
-              </Button>
-              <Button variant="outline-dark" className="m-2" onClick={logoutHandler}>
-                logout
-              </Button>
-              <NavLink to="/user/orders" className="btn btn-outline-dark m-2">
-                <i className="fa fa-user-plus mr-1"></i> My orders
-              </NavLink>
-              
-            </div>
-          ) : (
-            <div className="buttons text-center">
-              <NavLink to="/login" className="btn btn-outline-dark m-2">
-                <i className="fa fa-sign-in-alt mr-1"></i> Login
-              </NavLink>
-              <NavLink to="/register" className="btn btn-outline-dark m-2">
-                <i className="fa fa-user-plus mr-1"></i> Register
-              </NavLink>
-            </div>
+          {/* 右侧导航 */}
+          <div className="ml-auto">
+          {userInfo && userInfo.role === 'buyer' ? (
+            <NavDropdown className='' title={<span><i className="fa fa-user mr-3"></i>{userInfo.username}</span>} id='username'>
+            <NavDropdown.Item>
+                <NavLink className="nav-link" to="/user/orders">
+                  My orders
+                </NavLink>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <NavLink className="nav-link" onClick={logoutHandler}>
+                  Logout
+                </NavLink>
+              </NavDropdown.Item>
+          </NavDropdown>
+           ) : (
+            <Nav className='ml-auto'>
+            <Nav.Item>
+                <NavLink className="nav-link" to="/login">
+                <i className="fa fa-sign-in-alt mr-1"></i>
+                   Login
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink className="nav-link" to="/register">
+                <i className="fa fa-user-plus mr-1"></i>
+                   Register
+                </NavLink>
+              </Nav.Item>
+          </Nav>
           )}
-          {userInfo && userInfo.role === 'seller' && (
-            <div className="buttons text-center">
-              <NavLink to="/addProduct" className="btn btn-outline-dark m-2">
-                <i className="fa fa-user-plus mr-1"></i> addProduct
-              </NavLink>
-              <NavLink to="/seller" className="btn btn-outline-dark m-2">
-                <i className="fa fa-user-plus mr-1"></i> seller
-              </NavLink>
-            </div>
-          )}
+          </div>
         </BootstrapNavbar.Collapse>
+        </Container>
       </BootstrapNavbar>
     </div>
   );
 }
 
 export default Navbar;
-
-
-
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-
-
-// function Navbar() {
-//     const dispatch = useDispatch()
-//     const userLogin = useSelector((state) => state.userLogin)
-//     const { userInfo } = userLogin
-
-//     //退出函数
-//     const logoutHandler = () => {
-//     dispatch(logout())
-//   }
-
-//     return (
-//         <div>
-//             <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
-//             <div className="container">
-//                 <NavLink className="navbar-brand fw-bold fs-3 px-2" to="/"> Flash Sale</NavLink>
-//                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                     <span className="navbar-toggler-icon"></span>
-//                 </button>
-
-//                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                     <ul className="navbar-nav m-auto my-2 text-center">
-//                         <li className="nav-item">
-//                             <NavLink className="nav-link" to="/">Home </NavLink>
-//                         </li>
-//                         <li className="nav-item">
-//                             <NavLink className="nav-link" to="/products">Products</NavLink>
-//                         </li>
-//                         <li className="nav-item">
-//                             <NavLink className="nav-link" to="/todaysSale">Today's Sale</NavLink>
-//                         </li>
-//                     </ul>
-//                     { userInfo ? (
-//                         <div className="buttons text-center">
-
-//                             <i className="btn btn-outline-dark m-2">{userInfo.username}</i>
-                            
-//                         <i className="btn btn-outline-dark m-2" onClick={logoutHandler}>logout</i>
-//                   </div>
-//                     ) : (
-//                         <div className="buttons text-center">
-//                         <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Login</NavLink>
-//                         <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
-//                     </div>
-                    
-//                 )}
-//                     {userInfo && userInfo.role === "seller" && (
-//                     <div className="buttons text-center">
-//                         <NavLink to="/addProduct" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> addProduct</NavLink>
-//                         <NavLink to="/seller" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> seller</NavLink>
-//                     </div>
-//               )}
-//                 </div>
-//             </div>
-//         </nav>
-//         </div>
-        
-//     );
-// }
-
-// export default Navbar
