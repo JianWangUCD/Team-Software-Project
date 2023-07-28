192d16c0d15bc504f47728c62c66acd8e341f609
Qiongyi Zhang <zhangqiongyi97@163.com>
2023-07-28 12:22:53 +0100

    modify countdown

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -14,17 +14,19 @@ export default function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [isSaleStarted, setIsSaleStarted] = useState(false);
+  const [isSaleEnd, setIsSaleEnd] = useState(false);
   const [countdown, setCountdown] = useState(null);
 
   // 返回获取的状态值
-  const userLogin = useSelector( state  => state.userLogin)
+  const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
   const fetchProduct = async () => {
     try {
       const response = await axios.get(`${BASE_URL}/product-service/flashsale/products/${id}`);
       setProduct(response.data);
-      setIsSaleStarted(Date.now() >= new Date(response.data.saleStartTime));
+      // setIsSaleStarted(Date.now() >= new Date(response.data.saleStartTime));
+      // setIsSaleEnd(Date.now() <= new Date(response.data.saleEndTime && response.data.stock === 0));
     } catch (error) {
       console.error(error);
     }
@@ -32,9 +34,8 @@ export default function ProductPage() {
 
   const handleCheckout = async () => {
     try {
-      if (userInfo){
+      if (userInfo) {
         if (userInfo.role === "buyer") {
-          
           const response = await axios.get(
             `${BASE_URL}/order-service/flashsale/stock-check`,
             {
@@ -43,23 +44,23 @@ export default function ProductPage() {
               },
             }
           );
-            if (response.data){
-              // <MessageBox>You got the item</MessageBox>
-              navigate(`/checkout?userId=${userInfo.id}&productId=${product.id}`)
-            }else{
-              // <MessageBox>Sold out</MessageBox>
-              console.log("Sold out")
-            }          
-            // fetchProduct();
-            // // 进行一些成功提示或导航到订单详情页面等操作
-            // navigate(`/product/${product.id}`)
+          if (response.data) {
+            // <MessageBox>You got the item</MessageBox>
+            navigate(`/checkout?userId=${userInfo.id}&productId=${product.id}`)
+          } else {
+            // <MessageBox>Sold out</MessageBox>
+            console.log("Sold out")
+          }
+          // fetchProduct();
+          // // 进行一些成功提示或导航到订单详情页面等操作
+          // navigate(`/product/${product.id}`)
         } else {
           console.error('You are not a buyer');
           // <MessageBox>You are not a buyer</MessageBox>
         }
-      } else { 
+      } else {
         navigate("/login", { state: { from: `/product/${id}` } });
-    }
+      }
     } catch (error) {
       console.error(error);
     }
@@ -71,21 +72,28 @@ export default function ProductPage() {
 
   const checkSaleStatus = () => {
     if (product) {
-      setIsSaleStarted(Date.now() >= new Date(product.saleStartTime));
+      setIsSaleStarted(Date.now() >= new Date(product.saleStartTime) && Date.now() <= new Date(product.saleEndTime));
+      setIsSaleEnd(Date.now() >= new Date(product.saleEndTime || product.stock === 0));
     }
   };
 
-  
+
 
   useEffect(() => {
-
+    
     // 计算并更新倒计时时间
     const calculateCountdown = () => {
-      if (product && !isSaleStarted) {
-        const now = new Date().getTime();
+      const now = new Date().getTime();
+      if (product){
         const saleStartTime = new Date(product.saleStartTime).getTime();
-        const timeDifference = saleStartTime - now;
-
+        const saleEndTime = new Date(product.saleEndTime).getTime();
+        let timeDifference;
+        if (isSaleStarted && !isSaleEnd) { 
+          timeDifference = saleEndTime - now;
+        }else {
+          timeDifference = saleStartTime - now;
+        }
+        
         if (timeDifference > 0) {
           const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
           const hours = Math.floor(
@@ -112,53 +120,65 @@ export default function ProductPage() {
     const interval2 = setInterval(calculateCountdown, 1000);
 
     // Clean up the interval when the component unmounts to avoid memory leaks
-    return () => clearInterval(interval1, interval2);
-  }, [product, isSaleStarted, countdown]); // Add "product" as a dependency for the second useEffect hook
+    return () => {
+      clearInterval(interval1);
+      clearInterval(interval2);
+    };
+  }, [product, isSaleStarted, isSaleEnd]); // Add "product" as a dependency for the second useEffect hook
 
-  
-  if (!product){
+
+  if (!product) {
     return <div> Product Not Found</div>;
   }
 
-  
+
   return (
     <div className="container my-5 py-2">
-    <Link className = 'btn btn-dark my-3'  to='/products'> return </Link>
-    <div className="row">
-      <div className="col-md-6 col-sm-12 py-3">
-        <img
-          className="img-fluid"
-          src={product.img} alt={product.productName}
-          width="400px"
-          height="400px"
-        />
-      </div>
-      <div className="col-md-6 col-md-6 py-5">
-        <h4 className="text-uppercase text-muted">catogory</h4>
-        <h1 className="display-5">{product.productName}</h1>
+      <Link className='btn btn-dark my-3' to='/products'> return </Link>
+      <div className="row">
+        <div className="col-md-6 col-sm-12 py-3">
+          <img
+            className="img-fluid"
+            src={product.img} alt={product.productName}
+            width="400px"
+            height="400px"
+          />
+        </div>
+        <div className="col-md-6 col-md-6 py-5">
+          <h4 className="text-uppercase text-muted">catogory</h4>
+          <h1 className="display-5">{product.productName}</h1>
 
-        <h3 className="display-6  my-4">${product.price}</h3>
-        <p className="lead">{product.detail}</p>
-        <h3 className="lead">Sale Start: {format(new Date(product.saleStartTime), 'yyyy-MM-dd HH:mm:ss')}</h3>
-        
-        <p className="lead">Stock: {product.stock}</p>
+          <h3 className="display-6  my-4">${product.price}</h3>
+          <p className="lead">{product.detail}</p>
+          <h3 className="lead">Sale Start: {format(new Date(product.saleStartTime), 'yyyy-MM-dd HH:mm:ss')}</h3>
 
-        {countdown && 
+          <p className="lead">Stock: {product.stock}</p>
+
+          { isSaleStarted && !isSaleEnd ? (
+             countdown && (
+              <p className="lead">
+            Sale end in: {countdown.days} days, {countdown.hours} hours,{' '}
+            {countdown.minutes} minutes, {countdown.seconds} seconds
+          </p>
+             )
+          ) : (countdown && 
             <p className="lead">
               Sale starts in: {countdown.days} days, {countdown.hours} hours,{' '}
               {countdown.minutes} minutes, {countdown.seconds} seconds
-            </p>}
+            </p>
+            )
+          }
 
-        <button
+          <button
             className="btn btn-outline-dark"
             onClick={handleCheckout}
-            disabled={!isSaleStarted}
+            disabled={!isSaleStarted && isSaleEnd}
           >
-          Checkout
-        </button>
+            Checkout
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-  
+
   );
 }
